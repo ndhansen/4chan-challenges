642171ba9db9aa8d9c69de8e32c0ac6887fbc321
Nicholas Hansen <hoiois1996@gmail.com>
2017-01-09 21:46:34 +0100

    challenge 20 super simple fizzbuzz complete

---
+++ 20_FizzBuzz/fizzbuzz.py
@@ -0,0 +1,9 @@
+for i in range(1, 100):
+    if i % 3 != 0 and i % 5 != 0:
+        print(i, end="")
+    if i % 3 == 0:
+        print("Fizz", end="")
+    if i % 5 == 0:
+        print("Buzz", end="")
+    i += 1
+    print(", ", end="")
