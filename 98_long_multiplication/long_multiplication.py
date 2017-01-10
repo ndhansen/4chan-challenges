634a997d2362ec71d7a32fc3dc601758212e449f
Nicholas Hansen <hoiois1996@gmail.com>
2017-01-10 15:16:50 +0100

    completed challenge 98. Using python feels like cheating.

---
+++ 98_long_multiplication/long_multiplication.py
@@ -0,0 +1,8 @@
+import random
+
+#Doing this with python is practicaly cheating.
+
+num1 = random.randint(10**100, ((10**101)-1))
+num2 = random.randint(10**100, ((10**101)-1))
+
+print(num1 * num2)
